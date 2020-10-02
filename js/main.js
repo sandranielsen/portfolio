b67ffdb4c7a9c3fbd2eeb82eedfdb00a84bf2d7f
sandranielsen <55579623+sandranielsen@users.noreply.github.com>
2020-10-02 10:50:18 +0200

    Front page adjustments

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Transparent to solid coloured navigation bar //
 var nav = document.getElementById('nav');
 window.onscroll = function () {
     if (window.pageYOffset > 100) {
@@ -5,4 +6,13 @@ window.onscroll = function () {
     } else {
         nav.style.background = "transparent";
     }
+}
+
+
+// Scroll to top button // 
+mybutton = document.getElementById("myBtn");
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
