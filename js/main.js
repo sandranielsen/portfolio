b88e01d2e88f055608ab9306de7d9d737f22fd75
sandranielsen <55579623+sandranielsen@users.noreply.github.com>
2020-10-02 23:45:09 +0200

    Adding written content + other adjustments here and there

--- js/main.js
+++ js/main.js
@@ -1,18 +1,9 @@
 // Transparent to solid coloured navigation bar //
 var nav = document.getElementById('nav');
 window.onscroll = function () {
-    if (window.pageYOffset > 100) {
+    if (window.pageYOffset > 50) {
         nav.style.background = "#ffffff";
     } else {
         nav.style.background = "transparent";
     }
-}
-
-
-// Scroll to top button // 
-mybutton = document.getElementById("myBtn");
-
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
 }
