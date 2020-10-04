79a9993bf11b6f819ee86c89e73d506d49858d1c
sandranielsen <55579623+sandranielsen@users.noreply.github.com>
2020-10-04 21:03:56 +0200

    Portfolio pages

--- js/main.js
+++ js/main.js
@@ -1,9 +1,32 @@
 // Transparent to solid coloured navigation bar //
-var nav = document.getElementById('nav');
+let nav = document.getElementById('nav');
 window.onscroll = function () {
     if (window.pageYOffset > 50) {
         nav.style.background = "#ffffff";
     } else {
         nav.style.background = "transparent";
     }
+}
+
+//Active navigation element highlight on page scroll - source: https://codepen.io/dbilanoski/pen/LabpzG//
+const sections = document.querySelectorAll("section[id]");
+
+window.addEventListener("scroll", navigationHighlighter);
+
+function navigationHighlighter() {
+
+    let scrollY = window.pageYOffset;
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 100;
+        sectionId = current.getAttribute("id");
+        if (
+            scrollY > sectionTop &&
+            scrollY <= sectionTop + sectionHeight
+        ) {
+            document.querySelector(".navigation a[href*=" + sectionId + "]").classList.add("active");
+        } else {
+            document.querySelector(".navigation a[href*=" + sectionId + "]").classList.remove("active");
+        }
+    });
 }
